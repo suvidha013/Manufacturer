24004c7d97520d6ea252609231f441a6711a77ab
suvidha013 <tcstrainingspringboot@gmail.com>
2022-02-04 19:24:27 +0530

    changed

--- src/main/java/com/example/demo/entity/Supplier.java
+++ src/main/java/com/example/demo/entity/Supplier.java
@@ -3,7 +3,7 @@ package com.example.demo.entity;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
-import javax.persistence.Column;
+
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
